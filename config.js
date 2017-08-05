5b40bc6a74231fd522133ac45c54acd3a66d5ee4
Will (Lazer) <laz3erpenguin@gmail.com>
2017-08-05 15:37:11 +1200

    Added the config.js File!

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	token: ``,
+	maintainers: [`214210404082122754`],
+	prefix: "/",
+	playingQuotes: [
+		"Minecraft 1.12",
+		"with Lazer's Code",
+		"#MLG",
+		"/coding",
+		"on 1 shard"
+	],
+};
